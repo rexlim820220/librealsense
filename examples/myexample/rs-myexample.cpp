ab9f77cf18489e9222ad61f53cf82c05e8bae7c1
R07525087 <rexlim820220@gmail.com>
2019-06-07 15:02:18 +0800

    re-target the function in myexample
    Now this module can "transform .bag to .pcd"! cool huh?

--- examples/myexample/rs-myexample.cpp
+++ examples/myexample/rs-myexample.cpp
@@ -1,325 +1,194 @@
-#include <map>
-#include <chrono>
-#include <thread>
-#include <vector>
-#include <imgui.h>
-#include <sstream>
-#include "example.hpp"
-#include "imgui_impl_glfw.h"
+#include <example.hpp>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 #include <librealsense2/rs.hpp>
-#include <librealsense2/rs_advanced_mode.hpp>
+#include <pcl/filters/passthrough.h>
 
-static const int flags = ImGuiWindowFlags_NoCollapse
-       | ImGuiWindowFlags_NoScrollbar
-       | ImGuiWindowFlags_NoSavedSettings
-       | ImGuiWindowFlags_NoTitleBar
-       | ImGuiWindowFlags_NoResize
-       | ImGuiWindowFlags_NoMove;
 
-struct view {
-    std::string     serial_num;
-    rs2::frameset   frame;
+using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+
+struct state {
+    state() {
+        yaw      =  0,0;
+        pitch    =  0,0;
+        last_x   =  0.0;
+        last_y   =  0.0;
+        ml       =false;
+        offset_x = 0.0f;
+        offset_y = 0.0f;
+    }
+    bool ml;
+    double yaw;
+    double pitch;
+    double last_x;
+    double last_y;
+    float offset_x;
+    float offset_y;
+
 };
 
-void sync (rs2::device *, int);
-void init (window *);
-void button();
-void play(window , rs2::device, rs2::pipeline *);
-void pause (window ,rs2::device , rs2::pipeline &,int);
-void draw_seek_bar(rs2::playback& , int* , float2& , float );
-std::string pretty_time(std::chrono::nanoseconds duration);
+float3 colors[] { { 0.8f, 0.1f, 0.3f },
+                  { 0.1f, 0.9f, 0.5f },
+                };
 
-int main(int argc, char ** argv) try
-{
-    window app(1280, 960, "rs-myexample");
-    ImGui_ImplGlfw_Init(app, false);
+pcl_ptr points_to_pcl(const rs2::points& points);
+void register_glfw_callbacks(window& app, state& app_state);
+void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points);
 
-    // rs-multicam
-    rs2::context                ctx;
-    rs2::colorizer              colorizer;
-    std::vector<rs2::pipeline>  pipelines;
-    texture                     depth_image;
+int main(int argc, char **argv) {
+    rs2::config cfg;
+    cfg.enable_device_from_file("/media/adam/Transcend/20130603/D415/20190603_163655.bag");
+    rs2::pipeline pipe;
+    pipe.start(cfg);
 
-    std::size_t                 dev_id = 0;
-    auto         list = ctx.query_devices();
+    window app(1280, 720 , "PCL Example");
+    state app_state;
+    register_glfw_callbacks(app, app_state);
 
-    for (auto&& dev : list)
-    {
-        sync(&dev, dev_id);
-        rs2::pipeline pipe(ctx);
-        rs2::config cfg;
-        cfg.enable_device(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
-        pipe.start(cfg);
-        pipelines.emplace_back(pipe);
-        dev_id++;
+    rs2::pointcloud pc;
+    rs2::points point;
+    rs2::frameset frames;
+    std::vector<pcl_ptr> layers;
+
+    if (pipe.poll_for_frames(&frames)) {
+        rs2::frame depth = frames.get_depth_frame();
+        point = pc.calculate(depth);
+        auto pcl_points = points_to_pcl(point);
+        pcl_ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PassThrough<pcl::PointXYZ> pass;
+        pass.setInputCloud(pcl_points);
+        pass.setFilterFieldName("z");
+        pass.setFilterLimits(0.0,1.0);
+        pass.filter(*cloud_filtered);
+
+        layers.push_back(pcl_points);
+        layers.push_back(cloud_filtered);
+        pcl::io::savePCDFile("/home/adam/Desktop/3655.pcd", *pcl_points, false);
     }
 
-    // rs-record
-
-    bool recorded = false;
-    bool recording = false;
-    int seek_pos;
-
-    std::vector<std::thread> threads;
-    while (app)
-    {
-        init(&app);
-        std::vector<view> new_views;
-
-        for (auto &&pipe : pipelines)
-        {
-            rs2::device device = pipe.get_active_profile().get_device();
-            std::string serial = device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
-
-            if (!device.as<rs2::playback>()) {
-                view viewer;
-                viewer.serial_num = serial;
-                viewer.frame = pipe.wait_for_frames();
-                new_views.emplace_back(viewer);
-            }
-        }
-        button();
-        for (auto &&pipe : pipelines)
-        {
-            rs2::device device = pipe.get_active_profile().get_device();           
-            if (!device.as<rs2::playback>()) {
-                ImGui::SetCursorPos({ app.width() / 2 - 100, 3 * app.height() / 5 + 90});
-                ImGui::Text("Click 'record' to start recording");
-                ImGui::SetCursorPos({ app.width() / 2 - 100, 3 * app.height() / 5 + 110 });
-                if (ImGui::Button("record", { 50, 50 }))
-                {
-                    if (!device.as<rs2::recorder>())
-                    {
-                        pipe.stop();
-                        rs2::pipeline pipe;
-                        rs2::config cfg; // Declare a new configuration
-                        cfg.enable_record_to_file(device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) + std::string(".bag"));
-                        pipe.start(cfg); //File will be opened at this point
-                        device = pipe.get_active_profile().get_device();
-                    }
-                    else{
-                        device.as<rs2::recorder>().resume();
-                    }
-                    recording = true;
-                }
-            }
-            if (device.as<rs2::recorder>())
-            {
-                char message[20];
-                if (recording){
-                    ImGui::SetCursorPos({ app.width() / 2 - 100, 3 * app.height() / 5 + 60 });
-                    strcpy (message, "Recording to file ");
-                    strcat (message, device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
-                    strcat (message, ".bag");
-                    ImGui::TextColored({ 255 / 255.f, 64 / 255.f, 54 / 255.f, 1 }, message);
-                    memset(message, 0, sizeof(message));
-                }
-                ImGui::SetCursorPos({ app.width() / 2, 3 * app.height() / 5 + 110 });
-                if (ImGui::Button("pause\nrecord", { 50, 50 }))
-                {
-                    device.as<rs2::recorder>().pause();
-                    recording = false;
-                }
-
-                ImGui::SetCursorPos({ app.width() / 2 + 100, 3 * app.height() / 5 + 110 });
-                if (ImGui::Button(" stop\nrecord", { 50, 50 }))
-                {
-                    pipe.stop(); // Stop the pipeline that holds the file and the recorder
-                    rs2::pipeline pipe; //Reset the shared pointer with a new pipeline
-                    pipe.start(); // Resume streaming with default configuration
-                    device = pipe.get_active_profile().get_device();
-                    recorded = true; // Now we can run the file
-                    recording = false;
-                }
-            }
-
-            if (recorded) {
-                play(app, device, &pipe);
-            }
-
-            if (device.as<rs2::playback>())
-            {
-                pause(app,device,pipe,seek_pos);
-            }
-        }
-        ImGui::PopStyleColor(4);
-        ImGui::PopStyleVar();
-
-        ImGui::End();
-        ImGui::Render();
-
-        rs2::frame depth;
-        float w = app.width() ;
-        float h = app.height() ;
-        int counter = 0;
-        for (const auto& viewer : new_views)
-        {
-            float scale = counter * 0.25f;
-            depth = colorizer.process(viewer.frame.get_depth_frame());
-            //cout serial-num & timestamp
-            std::cout<<viewer.serial_num<<' '<<depth.get_timestamp()<<'\n';
-            depth_image.render(depth, {150 +  w * ( 0.25f - scale) , 500 - h * 0.25f , 200 + w * ( 0.5f - scale) ,  720 - h * (0.75f)});
-            counter += 1;
-            counter %= list.size();
-        }
-        for(auto &t:threads){
-            t.join();
-        }
+    /*
+    while(app) {
+        //draw_pointcloud(app, app_state, layers);
     }
+    */
 
     return EXIT_SUCCESS;
 }
-catch (const rs2::error & e)
+
+pcl_ptr points_to_pcl(const rs2::points& points)
 {
-    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
-    return EXIT_FAILURE;
-}
-catch (const std::exception & e)
-{
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-}
+    pcl_ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-void sync(rs2::device *dev, int dev_id) {
-    auto advanced_dev = dev->as<rs400::advanced_mode>();
-    auto advanced_sensors = advanced_dev.query_sensors();
-
-    bool depth_found = false;
-    bool color_found = false;
-
-    rs2::sensor depth_sensor;
-    rs2::sensor color_sensor;
-
-    for (auto&& sensor : advanced_sensors) {
-        std::string module_name = sensor.get_info(RS2_CAMERA_INFO_NAME);
-        if (module_name == "Stereo Module") {
-            depth_sensor = sensor;
-            depth_found = true;
-        }
-        else if (module_name == "RGB Camera") {
-            color_sensor = sensor;
-            color_found = true;
-        }
-    }
-
-    if (dev_id == 0) {
-        std::cout << "Setting " << dev_id << " to master!" << std::endl;
-        depth_sensor.set_option(RS2_OPTION_INTER_CAM_SYNC_MODE, 1);
-    } else {
-        std::cout << "Setting " << dev_id << " to slave!" << std::endl;
-        depth_sensor.set_option(RS2_OPTION_INTER_CAM_SYNC_MODE, 2);
-    }
-}
-
-void init (window *app){
-    ImGui_ImplGlfw_NewFrame(1);
-    ImGui::SetNextWindowSize({ app->width(), app->height() });
-    ImGui::Begin("app", nullptr, flags);
-}
-
-void button () {
-    ImGui::PushStyleColor(ImGuiCol_TextSelectedBg, { 1, 1, 1, 1 });
-    ImGui::PushStyleColor(ImGuiCol_Button, { 36 / 255.f, 44 / 255.f, 51 / 255.f, 1 });
-    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, { 40 / 255.f, 170 / 255.f, 90 / 255.f, 1 });
-    ImGui::PushStyleColor(ImGuiCol_ButtonActive, { 36 / 255.f, 44 / 255.f, 51 / 255.f, 1 });
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 12);
-
-}
-
-void play (window app,rs2::device device,rs2::pipeline *pipe) {
-    ImGui::SetCursorPos({ app.width() / 2 - 100, 4 * app.height() / 5 + 30 });
-    ImGui::Text("Click 'play' to start playing");
-    ImGui::SetCursorPos({ app.width() / 2 - 100, 4 * app.height() / 5 + 50});
-    if (ImGui::Button("play", { 50, 50 }))
+    auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    cloud->width = sp.width();
+    cloud->height = sp.height();
+    cloud->is_dense = false;
+    cloud->points.resize(points.size());
+    auto ptr = points.get_vertices();
+    for (auto& p : cloud->points)
     {
-        char message[100];
-        if (!device.as<rs2::playback>())
+        p.x = ptr->x;
+        p.y = ptr->y;
+        p.z = ptr->z;
+        ptr++;
+    }
+
+    return cloud;
+}
+
+void register_glfw_callbacks(window& app, state& app_state)
+{
+    app.on_left_mouse = [&](bool pressed)
+    {
+        app_state.ml = pressed;
+    };
+
+    app.on_mouse_scroll = [&](double xoffset, double yoffset)
+    {
+        app_state.offset_x += static_cast<float>(xoffset);
+        app_state.offset_y += static_cast<float>(yoffset);
+    };
+
+    app.on_mouse_move = [&](double x, double y)
+    {
+        if (app_state.ml)
         {
-            pipe->stop(); // Stop streaming with default configuration
-            rs2::pipeline pipe;
-            rs2::config cfg;
-            strcpy (message, device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
-            strcat (message, ".bag");
-            cfg.enable_device_from_file(message);
-            pipe.start(cfg); //File will be opened in read mode at this point
-            device = pipe.get_active_profile().get_device();
+            app_state.yaw -= (x - app_state.last_x);
+            app_state.yaw = std::max(app_state.yaw, -120.0);
+            app_state.yaw = std::min(app_state.yaw, +120.0);
+            app_state.pitch += (y - app_state.last_y);
+            app_state.pitch = std::max(app_state.pitch, -80.0);
+            app_state.pitch = std::min(app_state.pitch, +80.0);
         }
-        else
+        app_state.last_x = x;
+        app_state.last_y = y;
+    };
+
+    app.on_key_release = [&](int key)
+    {
+        if (key == 32) // Escape
         {
-            device.as<rs2::playback>().resume();
+            app_state.yaw = app_state.pitch = 0; app_state.offset_x = app_state.offset_y = 0.0;
         }
-    }
+    };
 }
 
-void pause (window app,rs2::device device, rs2::pipeline &pipe, int seek_pos) {
-    rs2::playback playback = device.as<rs2::playback>();
-    /*if (pipe->poll_for_frames(&frames)) // Check if new frames are ready
-    {
-        depth = color_map.process(frames.get_depth_frame()); // Find and colorize the depth data for rendering
-    }*/
 
-    // Render a seek bar for the player
-    float2 location = { app.width() / 4, 4 * app.height() / 5 + 110 };
-    draw_seek_bar(playback , &seek_pos, location, app.width() / 2);
-
-    ImGui::SetCursorPos({ app.width() / 2, 4 * app.height() / 5 + 50 });
-    if (ImGui::Button(" pause\nplaying", { 50, 50 }))
-    {
-        playback.pause();
-    }
-
-    ImGui::SetCursorPos({ app.width() / 2 + 100, 4 * app.height() / 5 + 50 });
-    if (ImGui::Button("  stop\nplaying", { 50, 50 }))
-    {
-        pipe.stop();
-        rs2::pipeline pipe;
-        pipe.start();
-        device = pipe.get_active_profile().get_device();
-    }
-}
-
-void draw_seek_bar(rs2::playback& playback, int* seek_pos, float2& location, float width)
+void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points)
 {
-    int64_t playback_total_duration = playback.get_duration().count();
-    auto progress = playback.get_position();
-    double part = (1.0 * progress) / playback_total_duration;
-    *seek_pos = static_cast<int>(std::max(0.0, std::min(part, 1.0)) * 100);
-    auto playback_status = playback.current_status();
-    ImGui::PushItemWidth(width);
-    ImGui::SetCursorPos({ location.x, location.y });
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 12);
-    if (ImGui::SliderInt("##seek bar", seek_pos, 0, 100, "", true))
+    // OpenGL commands that prep screen for the pointcloud
+    glPopMatrix();
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+    float width = app.width(), height = app.height();
+
+    glClearColor(153.f / 255, 153.f / 255, 153.f / 255, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    gluPerspective(60, width / height, 0.01f, 10.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    gluLookAt(0, 0, 0, 0, 0, 1, 0, -1, 0);
+
+    glTranslatef(0, 0, +0.5f + app_state.offset_y*0.05f);
+    glRotated(app_state.pitch, 1, 0, 0);
+    glRotated(app_state.yaw, 0, 1, 0);
+    glTranslatef(0, 0, -0.5f);
+
+    glPointSize(width / 640);
+    glEnable(GL_TEXTURE_2D);
+
+    int color = 0;
+
+    for (auto&& pc : points)
     {
-        //Seek was dragged
-        if (playback_status != RS2_PLAYBACK_STATUS_STOPPED) //Ignore seek when playback is stopped
+        auto c = colors[(color++) % (sizeof(colors) / sizeof(float3))];
+
+        glBegin(GL_POINTS);
+        glColor3f(c.x, c.y, c.z);
+
+        /* this segment actually prints the pointcloud */
+        for (int i = 0; i < pc->points.size(); i++)
         {
-            auto duration_db = std::chrono::duration_cast<std::chrono::duration<double, std::nano>>(playback.get_duration());
-            auto single_percent = duration_db.count() / 100;
-            auto seek_time = std::chrono::duration<double, std::nano>((*seek_pos) * single_percent);
-            playback.seek(std::chrono::duration_cast<std::chrono::nanoseconds>(seek_time));
+            auto&& p = pc->points[i];
+            if (p.z)
+            {
+                // upload the point and texture coordinates only for points we have depth data for
+                glVertex3f(p.x, p.y, p.z);
+                //std::cout<<p.x<<' '<<p.y<<' '<<p.z<<'\n';
+            }
         }
+
+        glEnd();
     }
-    std::string time_elapsed = pretty_time(std::chrono::nanoseconds(progress));
-    ImGui::SetCursorPos({ location.x + width + 10, location.y });
-    ImGui::Text("%s", time_elapsed.c_str());
-    ImGui::PopStyleVar();
-    ImGui::PopItemWidth();
-}
 
-std::string pretty_time(std::chrono::nanoseconds duration)
-{
-    using namespace std::chrono;
-    auto hhh = duration_cast<hours>(duration);
-    duration -= hhh;
-    auto mm = duration_cast<minutes>(duration);
-    duration -= mm;
-    auto ss = duration_cast<seconds>(duration);
-    duration -= ss;
-    auto ms = duration_cast<milliseconds>(duration);
-
-    std::ostringstream stream;
-    stream << std::setfill('0') << std::setw(hhh.count() >= 10 ? 2 : 1) << hhh.count() << ':' <<
-        std::setfill('0') << std::setw(2) << mm.count() << ':' <<
-        std::setfill('0') << std::setw(2) << ss.count();
-    return stream.str();
+    // OpenGL cleanup
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPopAttrib();
+    glPushMatrix();
 }
