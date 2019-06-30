fd7ace923c877dee7f1f1fdc458fcc13d4ced0d4
R07525087 <rexlim820220@gmail.com>
2019-06-30 15:45:31 +0000

    Update rotate-pointcloud.cpp

--- examples/pointcloud/variation/rotate-pointcloud.cpp
+++ examples/pointcloud/variation/rotate-pointcloud.cpp
@@ -11,36 +11,40 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/visualization/cloud_viewer.h>
 
+void transform(pcl::PointCloud <pcl::PointXYZ>::Ptr &);
+void visualize(pcl::PointCloud <pcl::PointXYZ>::Ptr &);
+
 int main(int argc, char * argv[])
 {
-
     pcl::PointCloud <pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    char fname[FILENAME_MAX] = "/home/a/Desktop/655.pcd";
+    char fname[FILENAME_MAX] = "/home/a/Desktop/e504.pcd";
     if(pcl::io::loadPCDFile(fname, *cloud) == -1){
         PCL_ERROR("Couldn't read pcd file.\n");
         return -1;
     }
-    Eigen::Vector4d centroid;
-    pcl::compute3DCentroid<pcl::PointXYZ> (*cloud, centroid);
-    pcl::demeanPointCloud <pcl::PointXYZ, double> (*cloud, centroid, *cloud);
-
-    pcl::visualization::CloudViewer viewer("CLoud  Viewer");
-
-    float theta;
-    int a = 0 , b = 1;
-    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
-    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed (new pcl::PointCloud<pcl::PointXYZ> ());
-
-    for(b = 1 ; ; b += 1) {
-        for(a = 0; a/b < 2; a += 1) {
-            theta = a * M_PI / b;
-            std::cout<<a<<' '<<b<<' '<<theta<<'\n';
-            trans.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitY()));
-            pcl::transformPointCloud ( *cloud , *transformed, trans);
-            viewer.showCloud (transformed);
-            //boost::this_thread::sleep (boost::posix_time::microseconds (1000000));
-        }
-    }
+    //transform(cloud);
+    visualize(cloud);
 
     return EXIT_SUCCESS;
+}
+
+void transform(pcl::PointCloud <pcl::PointXYZ>::Ptr &cloud) {
+    float theta;
+    int a = 4 , b = 3;
+    Eigen::Affine3f trans = Eigen::Affine3f::Identity();
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed (new pcl::PointCloud<pcl::PointXYZ> ());
+    theta = a * M_PI / b;
+    //trans.rotate (Eigen::AngleAxisf (-theta, Eigen::Vector3f::UnitY()));*/
+    trans.translation() << 0.5, 0.0, 0.0;
+    pcl::transformPointCloud ( *cloud , *transformed, trans);
+    //visualize(transformed);
+    pcl::io::savePCDFile("/home/a/Desktop/e504.pcd", *transformed, false);
+}
+
+void visualize(pcl::PointCloud <pcl::PointXYZ>::Ptr &cloud) {
+    pcl::visualization::CloudViewer viewer("CLoud  Viewer");
+    viewer.showCloud (cloud);
+    while(!viewer.wasStopped()){
+
+    }
 }
